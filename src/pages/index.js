d590f735d4533ee526f0db099cae2dadbfcaa741
triplestitch <triplestitch@naver.com>
2026-03-02 16:16:44 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,9 +31,9 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
-        ctaText={'shop now'}
+        title={'Latent becomes potential'}
+        subtitle={'Research, works, and experiments'}
+        ctaText={'enter'}
         ctaAction={goToShop}
       />
 
